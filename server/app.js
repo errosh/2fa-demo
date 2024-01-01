2f79bba26c6c4767e67cf07be4955ab85b759586
errosh <76802442+errosh@users.noreply.github.com>
2024-01-01 12:32:41 +0530

    retrive aws secrete

--- server/app.js
+++ server/app.js
@@ -7,6 +7,29 @@ const router = require('./routes/user');
 const path = require('path');
 const PORT = process.env.PORT || 8000;
 
+const AWS = require('aws-sdk');
+
+// Set the region for AWS SDK if it's not set in your environment variables
+AWS.config.update({ region: 'us-east-1' });
+
+const secretsManager = new AWS.SecretsManager();
+
+const getSecret = async () => {
+  try {
+    const params = {
+      SecretId: 'DATABASE_URI' // Replace with your secret ID or name
+    };
+
+    const data = await secretsManager.getSecretValue(params).promise();
+    const secretValue = JSON.parse(data.SecretString);
+    console.log('Retrieved secret:', secretValue);
+    return secretValue;
+  } catch (err) {
+    console.error('Error retrieving secret:', err);
+    throw err;
+  }
+};
+
 mongoose
   .connect(process.env.DATABASE_URI)
   .then(() => {
@@ -16,6 +39,10 @@ mongoose
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors({origin:['http://localhost:3000']}))
 app.use(express.json())
+app.get('/test',async(req, res)=>{
+  const test = await getSecret()
+  res.send(test)
+})
 app.use('/api',router)
 
 app.listen(PORT,()=>{
